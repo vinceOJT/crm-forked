3a6ad0bc5b18e4e5135ffbf3e4dda1803afc12e0
vinceOJT <jaca.ojt@gmail.com>
2026-02-25 23:54:57 +0800

    added server action cookies for signin signup

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,15 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import {db} from "@/db/drizzle" // your drizzle instance
+import { db } from "@/db/drizzle" // your drizzle instance
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-      emailAndPassword: { 
-    enabled: true, 
-  }, 
+    emailAndPassword: {
+        enabled: true,
+    },
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
     }),
+    plugins: [nextCookies()] // make sure this is the last plugin in the array
+
 });
