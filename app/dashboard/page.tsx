0d82efc2fcd46bc92b3efbecc848e9146950a20e
vinceOJT <jaca.ojt@gmail.com>
2026-02-27 04:51:20 +0800

    added card display for varius status and chat for timeline progress

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,22 @@
+import { SectionCards } from "@/components/section-cards";
+import { ChartAreaInteractive } from "@/components/ui/chart-area-interactive";
 
 
-import React from 'react'
 
-function DashboardPage() {
+export default function Dashboard() {
   return (
-    <div>hello hello hello hi</div>
+    <>
+      <div className="flex flex-1 flex-col">
+        <div className="@container/main flex flex-1 flex-col gap-2">
+          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <SectionCards />
+            <div className="px-4 lg:px-6">
+              <ChartAreaInteractive />
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
   )
 }
-
-export default DashboardPage
-
